3451f0633f04aeb72958705b5430e2cb45c017b2
A-Abdeen <ahmed.yahya18@gmail.com>
2021-01-22 11:05:59 +0300

    initial react-friendly commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,126 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <body id="page-top">
+        <section className="page-section" id="about">
+          <div className="container">
+            <div className="text-center">
+              <h2 className="section-heading text-uppercase">About</h2>
+              <h3 className="section-subheading text-muted">
+                Lorem ipsum dolor sit amet consectetur.
+              </h3>
+            </div>
+            <ul className="timeline">
+              <li>
+                <div className="timeline-image">
+                  <img
+                    className="rounded-circle img-fluid"
+                    src="assets/1.jpg"
+                    alt=""
+                  />
+                </div>
+                <div className="timeline-panel">
+                  <div className="timeline-heading">
+                    <h4>2009-2011</h4>
+                    <h4 className="subheading">My Humble Beginnings</h4>
+                  </div>
+                  <div className="timeline-body">
+                    <p className="text-muted">
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                      Sunt ut voluptatum eius sapiente, totam reiciendis
+                      temporibus qui quibusdam, recusandae sit vero unde, sed,
+                      incidunt et ea quo dolore laudantium consectetur!
+                    </p>
+                  </div>
+                </div>
+              </li>
+              <li className="timeline-inverted">
+                <div className="timeline-image">
+                  <img
+                    className="rounded-circle img-fluid"
+                    src="assets/2.jpg"
+                    alt=""
+                  />
+                </div>
+                <div className="timeline-panel">
+                  <div className="timeline-heading">
+                    <h4>March 2011</h4>
+                    <h4 className="subheading">An Child is Born</h4>
+                  </div>
+                  <div className="timeline-body">
+                    <p className="text-muted">
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                      Sunt ut voluptatum eius sapiente, totam reiciendis
+                      temporibus qui quibusdam, recusandae sit vero unde, sed,
+                      incidunt et ea quo dolore laudantium consectetur!
+                    </p>
+                  </div>
+                </div>
+              </li>
+              <li>
+                <div className="timeline-image">
+                  <img
+                    className="rounded-circle img-fluid"
+                    src="assets/3.jpg"
+                    alt=""
+                  />
+                </div>
+                <div className="timeline-panel">
+                  <div className="timeline-heading">
+                    <h4>December 2012</h4>
+                    <h4 className="subheading">Transition to Full Service</h4>
+                  </div>
+                  <div className="timeline-body">
+                    <p className="text-muted">
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                      Sunt ut voluptatum eius sapiente, totam reiciendis
+                      temporibus qui quibusdam, recusandae sit vero unde, sed,
+                      incidunt et ea quo dolore laudantium consectetur!
+                    </p>
+                  </div>
+                </div>
+              </li>
+              <li className="timeline-inverted">
+                <div className="timeline-image">
+                  <img
+                    className="rounded-circle img-fluid"
+                    src="assets/4.jpg"
+                    alt=""
+                  />
+                </div>
+                <div className="timeline-panel">
+                  <div className="timeline-heading">
+                    <h4>July 2014</h4>
+                    <h4 className="subheading">Phase Two Expansion</h4>
+                  </div>
+                  <div className="timeline-body">
+                    <p className="text-muted">
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                      Sunt ut voluptatum eius sapiente, totam reiciendis
+                      temporibus qui quibusdam, recusandae sit vero unde, sed,
+                      incidunt et ea quo dolore laudantium consectetur!
+                    </p>
+                  </div>
+                </div>
+              </li>
+              <li className="timeline-inverted">
+                <div className="timeline-image">
+                  <h4>
+                    Be Part
+                    <br />
+                    Of My
+                    <br />
+                    Story!
+                  </h4>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </section>
+      </body>
     </div>
   );
 }
